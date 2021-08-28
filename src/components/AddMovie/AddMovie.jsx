966889e21ce45049aaa54a3a2cdacee70fe6a930
Hassan Shahid <habibshahid2013@gmail.com>
2021-08-27 20:49:51 -0500

    added routes

---
+++ src/components/AddMovie/AddMovie.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function AddMovies() {
+
+
+
+    return (
+
+        <>
+        <h1>ADD MOVIES 🎥</h1>
+
+        </>
+    )
+}
+
+
+export default AddMovies;
---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function Details(){
+
+
+
+    return(
+
+        <>
+        <h1>Details 📜</h1>
+        </>
+    )
+}
+
+
+export default Details; 
