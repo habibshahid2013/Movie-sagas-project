a288b2f00f0838be79676aa1f75d09a1a0a7d6eb
Hassan Shahid <habibshahid2013@gmail.com>
2021-08-27 21:07:01 -0500

    Link is set up

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-function AddMovies() {
+function AddMovie() {
 
 
 
@@ -15,4 +15,4 @@ function AddMovies() {
 }
 
 
-export default AddMovies;
+export default AddMovie;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,33 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
 import Details from '../Details/Details';
+import AddMovie from '../AddMovie/AddMovie'
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
       <Router>        
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">MovieList</Link>
+            </li>
+            <li>
+              <Link to="/details">details</Link>
+            </li>
+            <li>
+              <Link to="/addmovie">Favorites</Link>
+            </li>
+          </ul>
+        </nav>
+
         <Route path="/" exact>
           <MovieList />
         </Route>
+
         <Route path="/details" exact>
           <Details />
         </Route>
@@ -19,12 +36,6 @@ function App() {
           <AddMovie />
         </Route>
 
-        
-        
-
-        {/* Details page */}
-
-        {/* Add Movie page */}
       </Router>
     </div>
   );
